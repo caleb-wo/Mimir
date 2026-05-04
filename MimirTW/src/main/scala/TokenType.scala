9c9c5fae84f9bc23b9daaa3d52a3510ea6633106
calebwolfe <caleb-wolfe@gmx.com>
2026-05-04 17:19:53 -0600

    CLINE: implement TokenType enum with single-character and multi-character token variants

--- MimirTW/src/main/scala/TokenType.scala
+++ MimirTW/src/main/scala/TokenType.scala
@@ -1,2 +1,24 @@
 package tree.walk.mimir
 
+enum TokenType:
+  // Single-character tokens
+  case LeftParen, RightParen, LeftBrace, RightBrace, Comma
+  case Minus, Plus, Slash, Star
+  case Hash
+
+  // One or two character operators
+  case Equal, Less, LessEqual, Greater, GreaterEqual
+  case Is, Isnt // Mimir's == and !=
+
+  // Literals
+  case Identifier, String, Integer, Float
+
+  // Keywords
+  case And, Or, Not
+  case Bind, If, Else, While, Process, Return
+  case True, False, Nil
+  case Print
+
+  case EOF
+
+end TokenType
