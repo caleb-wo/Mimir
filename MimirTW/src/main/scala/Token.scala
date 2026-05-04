16e0cc9855a36c45a5199c0e648cae8e923454bb
calebwolfe <caleb-wolfe@gmx.com>
2026-05-04 17:23:08 -0600

    Setup token

--- MimirTW/src/main/scala/Token.scala
+++ MimirTW/src/main/scala/Token.scala
@@ -1,2 +1,9 @@
 package tree.walk.mimir
 
+case class Token(
+  tokenType: TokenType,
+  lexeme: String,
+  literal: Any,
+  line: Int,
+):
+  override def toString: String = s"$tokenType $lexeme $literal"
