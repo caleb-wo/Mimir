d2d83499f8f864da5b337e3fa07b2e7de8d5df07
calebwolfe <caleb-wolfe@gmx.com>
2026-05-13 15:01:24 -0600

    Begin work on parser/ast

--- MimirTW/src/main/scala/Expr.scala
+++ MimirTW/src/main/scala/Expr.scala
@@ -1,2 +1,23 @@
 package tree.walk.mimir
 
+enum Expr:
+  case Binary(left: Expr, operator: Token, right: Expr)
+  case Grouping(expression: Expr)
+  case Literal(value: Any)
+  case Unary(operator: Token, right: Expr)
+
+  def printer: String = this match
+    case Binary(left, op, right) => parenthesize(op.lexeme, left, right)
+    case Grouping(expr)          => parenthesize("group", expr)
+    case Literal(null)           => "nil"
+    case Literal(value)          => value.toString
+    case Unary(op, right)        => parenthesize(op.lexeme, right)
+
+  private def parenthesize(name: String, exprs: Expr*): String =
+    val builder = StringBuilder()
+    builder.append("(").append(name)
+    for expr <- exprs do
+      builder.append(" ").append(expr.printer)
+    builder.append(")")
+    builder.toString
+end Expr
